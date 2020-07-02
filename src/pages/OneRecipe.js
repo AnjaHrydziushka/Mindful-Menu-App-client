eb785f29ad2d27df734897b9dc02b4af493b0cb6
AnjaHrydziushka <anja.hryd@gmail.com>
2020-07-02 12:04:35 +0200

    New selector, got data for 1 recipe by id

--- src/pages/OneRecipe.js
+++ src/pages/OneRecipe.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRecipes } from '../store/recipes/actions';
+import { selectRecipeById } from '../store/recipes/selectors';
 
 export default function OneRecipe() {
+    
+    const { id } = useParams();
+    const parsedId = parseInt(id);
+    const dispatch = useDispatch();
+
+    const oneRecipe = useSelector(selectRecipeById(parsedId));
+
+    console.log("ONE RECIPE?", oneRecipe)
+
+    useEffect(() => {
+        dispatch(fetchRecipes)
+    }, [])
+
     return (
         <div>
             Hello
--- src/store/recipes/selectors.js
+++ src/store/recipes/selectors.js
@@ -2,3 +2,10 @@ export function selectRecipes(reduxState) {
     // console.log("REDUX STATE:", reduxState.recipes)
     return reduxState.recipes
 }
+
+export function selectRecipeById(id) {
+    return (reduxState) => {
+        // console.log("NEW SELECTOR:", reduxState.recipes)
+        return reduxState.recipes.find(recipe => recipe.id === id)
+    }
+}
