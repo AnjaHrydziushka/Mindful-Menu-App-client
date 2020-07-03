5546089fa0f3db713b8405c2b0f4457adf17ef11
AnjaHrydziushka <anja.hryd@gmail.com>
2020-07-03 14:46:17 +0200

    Created component for Menu, added css animation

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import RecipesList from './pages/RecipesList';
 import { Switch, Route } from "react-router-dom";
 import OneRecipe from './pages/OneRecipe';
 import TagRecipes from './pages/TagRecipes';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
+      <Menu />
       <Switch>
         <Route exact path="/" component={RecipesList} />
         <Route path="/recipes/:id" component={OneRecipe} />
