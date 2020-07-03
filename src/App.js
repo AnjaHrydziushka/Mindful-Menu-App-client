cff697f9b4345a87d32d37ea5da6d50275d424c0
AnjaHrydziushka <anja.hryd@gmail.com>
2020-07-03 10:36:36 +0200

    New component for recipes displayed by tag

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import RecipesList from './pages/RecipesList';
 import { Switch, Route } from "react-router-dom";
 import OneRecipe from './pages/OneRecipe';
+import TagRecipes from './pages/TagRecipes';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={RecipesList} />
         <Route path="/recipes/:id" component={OneRecipe} />
+        <Route path="/:tag" component={TagRecipes} />
       </Switch>
     </div>
   );
