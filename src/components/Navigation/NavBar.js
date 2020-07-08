2dbe17d4dfe88e35f0e0b39db286d48be51fc3ed
AnjaHrydziushka <anja.hryd@gmail.com>
2020-07-08 17:54:30 +0200

    Changed NavBar css

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Navbar }from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
+import './Menu.css'
 
 export default function NavBar() {
     const history = useHistory();
+    const navbar = {backgroundColor: "lightskyblue"}
     return (
         <div>
-            <Navbar bg="light">
-                <Navbar.Brand onClick={() => history.push('/')}>Mindful App</Navbar.Brand>
-            </Navbar>
+            <nav class="navbar navbar-light" style={navbar}>
+                <a class="navbar-brand" onClick={() => history.push('/')}>Mindful Menu</a>
+            </nav>
         </div>
     )
 }
