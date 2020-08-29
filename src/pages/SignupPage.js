e517f6deb594d026a28017639f0001915f0806cf
AnjaHrydziushka <anja.hryd@gmail.com>
2020-08-29 14:29:00 +0200

    Event listeners for Signup

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SignupPage() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("Email:", email, "Password:", password);
+    }
     return (
         <div>
             <h1>Signup</h1>
-                <form>
-                    <label>Email</label>
-                    <input type="email" />
-                    <label>Password</label>
-                    <input type="password" />
+                <form onSubmit={handleSubmit}>
+                    <p>
+                    <label>
+                        Email:{" "}
+                        <input 
+                        type="email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        />
+                        </label>
+                    </p>
+                    <p>
+                    <label>
+                        Password:{" "}
+                        <input
+                        type="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        />
+                        </label>
+                    </p>
+                    <button type="submit">Sign up</button>
                 </form>
         </div>
     )
